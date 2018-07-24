1a1106c7754a9f3cc56692b2e4f95f1dbda3036e
Antti Kuukka <akuukka@gmail.com>
2018-07-24 23:40:47 +0300

    Initial commit

---
+++ git_autobuild.py
@@ -0,0 +1,106 @@
+import time
+import signal
+import sys
+import os
+import json
+import subprocess
+
+config = {
+    "update_interval": 30.0,
+    "repo": "https://bitbucket.org/akuukka/helherron",
+    "working_dir": "temp",
+    "run_cmd": "python3 build.py"
+}
+
+quit = False
+
+def signal_handler(signal, frame):
+    global quit
+    print("Requesting quit...")
+    quit = True
+
+def init():
+    print("Initializing...")
+    data = { "last_hash": 0 }
+    with open('.git_autobuild', 'w') as outfile:
+        json.dump(data, outfile)
+    outfile.close()
+
+    # Clone the repo
+    cmd = "git clone " + config["repo"]
+    res = os.system(cmd)
+
+def get_last_processed_hash():
+    outfile = open('.git_autobuild', 'r')
+    data = json.load(outfile)
+    outfile.close()
+    return str(data["last_hash"])
+
+def get_repo_dir():
+    dirlist = os.listdir(".")
+    for dir in dirlist:
+        if dir in config["repo"]:
+            return dir
+    print("Could not find repo dir")
+    exit(2)
+
+def get_current_hash():
+    repo_dir = get_repo_dir()
+    os.chdir(repo_dir)
+    os.system("git rev-parse HEAD > ../.hash_temp")
+    os.chdir("..")
+
+    hash_file = open(".hash_temp", 'r')
+    hash_str = hash_file.read().strip()
+    hash_file.close()
+    
+    return hash_str
+
+def do_pull():
+    repo_dir = get_repo_dir()
+    os.chdir(repo_dir)
+    subprocess.check_output('git pull', shell=True)
+    os.chdir("..")
+
+def process_repo(new_hash):
+    repo_dir = get_repo_dir()
+    os.chdir(repo_dir)
+    os.system(config["run_cmd"])
+    os.chdir("..")
+
+    # Update processed hash
+    outfile = open('.git_autobuild', 'r')
+    data = json.load(outfile)
+    outfile.close()
+    data["last_hash"] = new_hash
+    with open('.git_autobuild', 'w') as outfile:
+        json.dump(data, outfile)
+    outfile.close()    
+
+if __name__ == "__main__":
+    signal.signal(signal.SIGINT, signal_handler)
+
+    print("Starting git-autobuild")
+
+    wd = config["working_dir"]
+
+    # TODO: if clean is requested, should remove the working directory if it exists    
+    if not os.path.exists(wd):
+        os.mkdir(wd)
+
+    os.chdir(wd)
+    if not os.path.exists(".git_autobuild"):
+        init()
+
+    update_interval = config["update_interval"]
+    while not quit:
+        do_pull()
+        
+        last_processed_hash = get_last_processed_hash()
+        current_hash = get_current_hash()
+        if last_processed_hash != current_hash:
+            print("Top commit changed - trigger build")
+            process_repo(current_hash)
+            
+        time.sleep(update_interval)
+    
